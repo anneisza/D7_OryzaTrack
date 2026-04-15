a142e648c5e9b66196ebfe43ba8248697ea8b9cb
anneisza <rizaanneisza@gmail.com>
2026-04-15 20:45:06 +0700

    Mengisi Class FormRiwayatPenyakit

--- OryzaTrack/FormRiwayatPenyakit.cs
+++ OryzaTrack/FormRiwayatPenyakit.cs
@@ -13,9 +13,39 @@ namespace OryzaTrack
 {
     public partial class FormRiwayatPenyakit : Form
     {
-        public FormRiwayatPenyakit()
+        private RiwayatPenyakitBLL bll = new RiwayatPenyakitBLL();
+        private int _idAdmin;
+        private int _selectedId = 0;
+
+        public FormRiwayatPenyakit(int idAdmin)
         {
             InitializeComponent();
+            _idAdmin = idAdmin;
+        }
+
+        private void FormRiwayatPenyakit_Load(object sender, EventArgs e)
+        {
+            cmbTingkatKerusakan.Items.AddRange(new string[] { "Ringan", "Sedang", "Berat", "Sangat Berat" });
+            cmbTingkatKerusakan.SelectedIndex = 0;
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            dgvPenyakit.DataSource = bll.GetAll();
+            lblJumlah.Text = "Jumlah Data: " + bll.CountData();
+        }
+
+        private void dgvPenyakit_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            DataGridViewRow row = dgvPenyakit.Rows[e.RowIndex];
+            _selectedId = Convert.ToInt32(row.Cells["idPenyakit"].Value);
+            txtIdPenyakit.Text = _selectedId.ToString();
+            txtGejalaPenyakit.Text = row.Cells["gejalaPenyakit"].Value?.ToString();
+            cmbTingkatKerusakan.Text = row.Cells["tingkatKerusakan"].Value?.ToString();
+            txtLokasiLahan.Text = row.Cells["lokasiLahan"].Value?.ToString();
+            dtpTanggalSerangan.Value = Convert.ToDateTime(row.Cells["tanggalSerangan"].Value);
         }
     }
 }
